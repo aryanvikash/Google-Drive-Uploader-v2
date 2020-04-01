06166bf5c031b3e512eb5d3ed44fd34a43852cfc
aryanvikash <followvikash8@gmail.com>
2020-04-01 19:50:41 +0530

    bot token

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 
 DOWNLOAD_LOCATION = "./Downloads"
 # BOT_TOKEN = "676967834:AAFj5Tff1BcZYtNVPfLZAd9KCNegB3TnI1k"
-# BOT_TOKEN = "955851494:AAFkpvw9al87ipPVJU9i28eyfTN86dAclkI" #gdriveupme_bot
-BOT_TOKEN = "988394638:AAEck5ZsVByoKR4syncTw01q2mwLoyuKHm0"  #  @aryantest_bot
+BOT_TOKEN = "955851494:AAFkpvw9al87ipPVJU9i28eyfTN86dAclkI" #gdriveupme_bot
+# BOT_TOKEN = "988394638:AAEck5ZsVByoKR4syncTw01q2mwLoyuKHm0"  #  @aryantest_bot
 APP_ID = "1278490"
 API_HASH = "69d16301a6ce2d3b68b81e5f881bfca7"
 AUTH_GROUP = ""
