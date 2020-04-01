e4f0720490643b88f9c10ec00402b2998cc50f47
aryanvikash <followvikash8@gmail.com>
2020-04-01 19:33:28 +0530

    possibe changed to DC1

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 
 DOWNLOAD_LOCATION = "./Downloads"
 # BOT_TOKEN = "676967834:AAFj5Tff1BcZYtNVPfLZAd9KCNegB3TnI1k"
-BOT_TOKEN = "955851494:AAFkpvw9al87ipPVJU9i28eyfTN86dAclkI" #gdriveupme_bot
-# BOT_TOKEN = "988394638:AAEck5ZsVByoKR4syncTw01q2mwLoyuKHm0"  #  @aryantest_bot
-APP_ID = "886360"
-API_HASH = "db0bd1cae95560f1ff4f397385fd55db"
+# BOT_TOKEN = "955851494:AAFkpvw9al87ipPVJU9i28eyfTN86dAclkI" #gdriveupme_bot
+BOT_TOKEN = "988394638:AAEck5ZsVByoKR4syncTw01q2mwLoyuKHm0"  #  @aryantest_bot
+APP_ID = "1278490"
+API_HASH = "69d16301a6ce2d3b68b81e5f881bfca7"
 AUTH_GROUP = ""
 
 Creds_path = "/app/creds/"
