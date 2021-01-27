105e4cb2bb8ca2a35097be050e8dd0bf74cc6482
Aryan Vikash <followvikash8@gmail.com>
2021-01-27 08:15:05 +0000

    env varible changed

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
+import os
+
 DOWNLOAD_LOCATION = "./Downloads"
-
-BOT_TOKEN = "955851494:AAFkpvw9al87ipPVJU9i28eyfTN86dAclkI" #gdriveupme_bot
+BOT_TOKEN = os.environ.get("BOT_TOKEN","955851494:AAFkpvw9al87ipPVJU9i28eyfTN86dAclkI")
 # BOT_TOKEN = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"  #  @aryantest_bot
-
-APP_ID = "2861792"
-API_HASH = "70d5679ed956d7832f8dd462643f7028"
+APP_ID = int(os.environ.get("API_ID","2861792"))
+API_HASH = os.environ.get("API_HASH","70d5679ed956d7832f8dd462643f7028")
 AUTH_GROUP = ""
 
 Creds_path = "creds"
